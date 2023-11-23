c7469b81b1bcdefa1c5590125c4be19ce5e5b4ca
JalalSayed1 <mrjalalsayed@gmail.com>
2023-11-23 13:38:22 +0000

    Controlling servo using joystick

--- joystick_controller_LED.py
+++ joystick_controller_LED.py
@@ -1,4 +1,4 @@
-from pyfirmata2 import Arduino
+from pyfirmata2 import Arduino, SERVO
 import time
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -32,6 +32,9 @@ class RGB_LED_Controller:
         self.ax_freq.set_xlabel('Frequency (Hz)')
         self.ax_freq.set_ylabel('Amplitude')
         self.ax_freq.legend()
+        self.ax_freq.set_xlim(0, 50)
+        # self.ax_freq.set_xlim(0, 50)#!samplingRate / 2)
+        self.ax_freq.set_ylim(0, 100)
 
         # Ringbuffers for accumulating samples
         self.ringbuffer0 = []
@@ -55,14 +58,24 @@ class RGB_LED_Controller:
         # Update frequency domain plot for both channels
         fft_vals0 = np.fft.fft(self.plotbuffer0)
         fft_vals1 = np.fft.fft(self.plotbuffer1)
+        
+        # boost some noise frequencies to make them visible in the plot. 20 to 50hz:
+        # low_index = int(20 * len(fft_vals0) / samplingRate)
+        # high_index = int(50 * len(fft_vals0) / samplingRate)
+        # fft_vals0[low_index:high_index] *= 100
+        # fft_vals1[low_index:high_index] *= 100
+        # fft_vals0[0:low_index] = 0
+        # fft_vals1[0:low_index] = 0
+        
+        
         fft_freq = np.fft.fftfreq(len(fft_vals0), 1 / samplingRate)
         mask = fft_freq > 0
 
         self.line_freq0.set_data(fft_freq[mask], np.abs(fft_vals0[mask]))
         self.line_freq1.set_data(fft_freq[mask], np.abs(fft_vals1[mask]))
-        self.ax_freq.set_xlim(0, samplingRate / 2)
-        max_amplitude = max(max(np.abs(fft_vals0[mask])), max(np.abs(fft_vals1[mask]))) + 10
-        self.ax_freq.set_ylim(0, max_amplitude)
+        
+        # max_amplitude = max(max(np.abs(fft_vals0[mask])), max(np.abs(fft_vals1[mask]))) + 10
+        
 
         return self.line_freq0, self.line_freq1
 
@@ -75,19 +88,20 @@ class RGB_LED_Controller:
             
     def print_values(self, pin, value):
         print(f"pin: {pin} \t value: {value}")
+        
+    def write_pwm(self, pwm, value):
+        # get angle form value:
+        # angle = int(value * 180)
+        # write angle to servo:
+        # pwm.write(angle)
+        
+        pwm.write(value)
 
 
-# Sampling rate: 500Hz
-samplingRate = 500
+samplingRate = 200
 
 # Create an instance of the animated scrolling window
 rgb_led_controller = RGB_LED_Controller()
-
-def callBack(pin, value):
-    rgb_led_controller.print_values(pin, value)
-    # Callback for new samples from the Arduino
-    rgb_led_controller.addData(value, pin)
-    pwm.write(value)
 
 # Get the Arduino board
 board = Arduino(PORT)
@@ -101,6 +115,26 @@ y_axis_pin = 1 # A1
 sw_pin = 7 # D7
 pwm_pin = 6 # D6
 
+# pwm = board.digital[pwm_pin]
+# pwm.mode = SERVO
+pwm = board.get_pin(f'd:{pwm_pin}:p')
+
+
+def callBack(pin, value):
+    # rgb_led_controller.print_values(pin, value)
+    # Callback for new samples from the Arduino
+    rgb_led_controller.addData(value, pin)
+    
+    rgb_led_controller.write_pwm(pwm, value)
+    
+    # pwm.write(value)
+    # time.sleep(0.01)
+    # pwm.write(0)
+
+
+
+
+
 # Register callbacks for both analog pins
 board.analog[x_axis_pin].register_callback(lambda value, pin=x_axis_pin: callBack(pin, value))
 board.analog[x_axis_pin].enable_reporting()
@@ -108,7 +142,9 @@ board.analog[y_axis_pin].register_callback(lambda value, pin=y_axis_pin: callBac
 board.analog[y_axis_pin].enable_reporting()
 
 # pwm signal from digital pin 8:
-pwm = board.get_pin(f'd:{pwm_pin}:p')
+# pwm = board.get_pin(f'd:{pwm_pin}:p')
+# pwm = board.digital[pwm_pin]
+# pwm.mode = SERVO
 
 
 # Show the plot and start the animation

c941f59e102499570823b1268950ba89c4a486d2
JalalSayed1 <mrjalalsayed@gmail.com>
2023-11-30 19:15:49 +0000

    new code

--- joystick.py
+++ joystick.py
@@ -23,12 +23,18 @@ board = Arduino(PORT)
 board.digital[pin_red_led].mode = PWM
 board.digital[pin_green_led].mode = PWM
 
+current_x_pwm_value = 0
+current_y_pwm_value = 0
+
 # Function to map values
 def map_value(value, in_min, in_max, out_min, out_max):
     return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 # Function to update LED brightness based on joystick values
 def update_led():
+    global current_x_pwm_value
+    global current_y_pwm_value
+    
     x_value = board.analog[pin_x_axis].read()  # Read X axis value
     y_value = board.analog[pin_y_axis].read()  # Read Y axis value
     
@@ -38,16 +44,46 @@ def update_led():
         # Map the joystick values to PWM range (0-255)
         # pwm_x = int(map_value(x_value, 0, 1, 0, 255))
         # pwm_y = int(map_value(y_value, 0, 1, 0, 255))
-        pwm_x = int(map_value(x_value, 0, 1, 1, 100))
-        pwm_y = int(map_value(y_value, 0, 1, 1, 100))
+        # if x_value < 0.01:
+        #     x_value = 0
+        # elif y_value < 0.01:
+        #     y_value = 0
+        # elif x_value > 0.99:
+        #     x_value = 1
+        #     y_value = 0
+        # elif y_value > 0.99:
+        #     x_value = 0
+        #     y_value = 1
+            
+        pwm_x = int(map_value(x_value, 0, 1, 0, 100))
+        pwm_y = int(map_value(y_value, 0, 1, 0, 100))
+        
+        # Adjust intensity inversely for smooth transition
+        if pwm_x > current_x_pwm_value:
+            pwm_y = 100 - pwm_y
+        elif pwm_y > current_y_pwm_value:
+            pwm_x = 100 - pwm_x
+        # elif pwm_x < current_x_pwm_value:
+        #     pwm_y = 100 - pwm_y
+        # elif pwm_y < current_y_pwm_value:
+        #     pwm_x = 100 - pwm_x
+            
+        # pwm_red = pwm_x
+        # pwm_green = pwm_y
+        
+        print(f"\t\t Writing pwm_x: {pwm_x} \t pwm_y: {pwm_y}")
 
         # Update the LED brightness
         board.digital[pin_red_led].write(pwm_x)
         board.digital[pin_green_led].write(pwm_y)
+        
+        # update current pwm values
+        current_x_pwm_value = pwm_x
+        current_y_pwm_value = pwm_y
 
 # Function to animate the LED brightness
-def animate(i):
-    update_led()
+# def animate(i):
+#     update_led()
     
 def callBack(pin, value):
     update_led()
@@ -57,7 +93,7 @@ sampling_rate = 200
 board.samplingOn(1000 / sampling_rate)
 
 # Set up animation
-ani = animation.FuncAnimation(plt.gcf(), animate, interval=50)
+# ani = animation.FuncAnimation(plt.gcf(), animate, interval=50)
 
 board.analog[pin_x_axis].register_callback(lambda value, pin=pin_x_axis: callBack(pin, value))
 board.analog[pin_x_axis].enable_reporting()
@@ -66,7 +102,9 @@ board.analog[pin_y_axis].enable_reporting()
 
 try:
     # Show the plot (this will not close automatically)
-    plt.show()
+    # plt.show()
+    while True:
+        time.sleep(0.1)
     
 except KeyboardInterrupt as e:
     print("KeyboardInterrupt")

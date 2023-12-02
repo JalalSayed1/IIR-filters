6d4ec4ff92b2c53d2e4c114bbd4f81fd60459a8f
Jalal Sayed <mrjalalsayed@gmail.com>
2023-12-02 09:44:05 +0000

    Plot both time and frequency domain

--- joystick.py
+++ joystick.py
@@ -1,158 +1,106 @@
 from pyfirmata2 import Arduino, PWM
-import time
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 
+# Constants
 PORT = Arduino.AUTODETECT
+BUFFER_SIZE = 500
+SAMPLING_RATE = 200
+
+# Arduino Setup
+board = Arduino(PORT)
+board.samplingOn(1000 / SAMPLING_RATE)
 
 # Define the pins
-pin_x_axis = 0  # A0 for X axis
-pin_y_axis = 1  # A1 for Y axis
+pin_x_axis = 0  # Analog pin A0 for X axis
 led_red_pin = 5
 led_blue_pin = 6
-# led_brightness_pin = 5
-
-# Establish a connection to the Arduino board
-board = Arduino(PORT)
 
 # Set the pins as PWM output
 board.digital[led_red_pin].mode = PWM
 board.digital[led_blue_pin].mode = PWM
 
-# Function to map values
-def map_value(value, in_min, in_max, out_min, out_max):
-    return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+# Function to update LED color based on joystick values
+def update_led_color(pin_value):
+    duty_cycle_red = interpolate(pin_value, 0, 1, 0, 1)
+    duty_cycle_blue = 1 - duty_cycle_red
+
+    board.digital[led_red_pin].write(duty_cycle_red)
+    board.digital[led_blue_pin].write(duty_cycle_blue)
 
 # Function to interpolate values
 def interpolate(val, start_orig, end_orig, start_target, end_target):
     return ((val - start_orig) / (end_orig - start_orig)) * (end_target - start_target) + start_target
 
-# Function to update LED brightness based on joystick values
-def update_led_colour(pin_value):
-    if pin_value < 0.01:
-        print("## 1")
-        duty_cycle_5 = 0.0
-        duty_cycle_6 = 1.0
-    elif pin_value > 0.99:
-        print("## 2")
-        duty_cycle_5 = 1.0
-        duty_cycle_6 = 0.0
-    else:
-        # Smoothly transition both duty cycles based on pin_value
-        duty_cycle_5 = interpolate(pin_value, 0, 1, 0, 1)
-        duty_cycle_6 = 1 - duty_cycle_5
+# Initialize Data Buffers
+time_domain_data = np.zeros(BUFFER_SIZE)
 
-    board.digital[led_red_pin].write(duty_cycle_5)
-    board.digital[led_blue_pin].write(duty_cycle_6)
-    
-    print(f"Red: {duty_cycle_5 * 100}% \t Blue: {duty_cycle_6 * 100}")
+# Initialize Figures
+fig, (ax_time, ax_freq) = plt.subplots(2, 1,)
 
-def update_led_brightness(pin_value):
-    pwm_value = int(map_value(pin_value, 0, 1, 0, 100))
-    board.digital[led_brightness_pin].write(pwm_value)
-    print(f"Brightness: {pwm_value}%")
-    
+# Time Domain Plot
+ax_time.set_xlabel('Time')
+ax_time.set_ylabel('X-axis Value')
+ax_time.set_title('Time Domain')
+ax_time.set_ylim(-0.1, 1.1)
+line_time, = ax_time.plot(np.arange(BUFFER_SIZE), time_domain_data, label='X-axis value')
 
-# Function to animate the LED brightness
-# def animate(i):
-#     update_led()
+# Frequency Domain Plot
+ax_freq.set_xlabel('Frequency')
+ax_freq.set_ylabel('Amplitude')
+ax_freq.set_title('Frequency Domain')
+line_freq, = ax_freq.plot([], [], label='X-axis Value (Frequency Domain)')
+ax_freq.set_xlim(0, SAMPLING_RATE / 2)
+ax_freq.set_ylim(0, 50)
+ax_freq.set_xticks(np.arange(0, SAMPLING_RATE/2 + 1, 10))
 
-#' plotting
-# Create lists to store data for plotting
-time_vals = []
-pin_vals = []
-
-# Set up the plot
-fig, ax = plt.subplots()
-line, = ax.plot([], [], 'b-', label='Pin Value')
-ax.set_xlim(0, 10)  # Set initial x-axis limits
-ax.set_ylim(-0.5, 1.5)   # Set y-axis limits (adjust as needed)
-ax.set_xlabel('Time')
-ax.set_ylabel('Pin Value')
-ax.legend()
-
-# Function to update the plot data
+# Function to Update Plots
 def update_plot(frame):
-    global time_vals, pin_vals
+    global time_domain_data
 
-    # Read data from the pin (modify this according to your setup)
-    pin_value = board.analog[pin_x_axis].read()
+    # Update Time Domain Data
+    new_data = board.analog[pin_x_axis].read()  # Read new data from Arduino
+    if new_data is not None:
+        time_domain_data = np.roll(time_domain_data, -1)
+        time_domain_data[-1] = new_data
+        line_time.set_ydata(time_domain_data)
 
-    # Append values to the lists
-    current_time = time.time()
-    time_vals.append(current_time)  # Use current time for x-axis
-    pin_vals.append(pin_value)
-    
-    # Filter and limit the data to display in the plot
-    time_window = 10  # Set the time window to display (adjust as needed)
-    time_vals = [t for t in time_vals if current_time - t <= time_window]
-    pin_vals = pin_vals[-len(time_vals):]
+        # Update Frequency Domain Data
+        fft_data = np.fft.fft(time_domain_data)
+        fft_freq = np.fft.fftfreq(BUFFER_SIZE, 1 / SAMPLING_RATE)
+        mask = fft_freq > 0
+        line_freq.set_data(fft_freq[mask], np.abs(fft_data[mask]))
 
-    # Update the plot with new data
-    line.set_data(time_vals, pin_vals)
+    return line_time, line_freq
 
-    # Update x-axis limits to display the most recent data
-    if time_vals:
-        ax.set_xlim(time_vals[0], time_vals[-1])
-
-    return line,
-
-# Function to initialize the plot
+# Function to Initialize the Plot
 def init():
-    line.set_data([], [])
-    return line,
+    line_time.set_ydata(np.zeros(BUFFER_SIZE))
+    line_freq.set_data([], [])
+    return line_time, line_freq
 
-# Set up the animation
-ani = animation.FuncAnimation(fig, update_plot, frames=np.linspace(0, 10, 100),
-                              init_func=init, blit=True, interval=100)
+# Create Animation
+ani = animation.FuncAnimation(fig, update_plot, init_func=init, blit=True, interval=1)
 
-#'------------
+# Arduino Callback for LED Update
+def joystick_callback(value):
+    update_led_color(value)
 
-def callBack(pin, value):
-
-    if pin == 0:
-        print(f"pin: {pin} \t value: {value}")
-        update_led_colour(value)
-        
-        
-    # elif pin == 1:
-        # update_led_brightness(value)
-        # pass
-    
-
-sampling_rate = 200
-board.samplingOn(1000 / sampling_rate)
-
-# Set up animation
-# ani = animation.FuncAnimation(plt.gcf(), animate, interval=50)
-
-board.analog[pin_x_axis].register_callback(lambda value, pin=pin_x_axis: callBack(pin, value))
+board.analog[pin_x_axis].register_callback(joystick_callback)
 board.analog[pin_x_axis].enable_reporting()
-board.analog[pin_y_axis].register_callback(lambda value, pin=pin_y_axis: callBack(pin, value))
-board.analog[pin_y_axis].enable_reporting()
 
+# Show Plot
 try:
-    # Show the plot (this will not close automatically)
+    plt.tight_layout()
     plt.show()
-    # while True:
-    #     time.sleep(1)
     
-except KeyboardInterrupt as e:
-    print("KeyboardInterrupt")
-    # print(e)
+except KeyboardInterrupt:
+    print("Interrupted by user")
 
 finally:
-    print("Terminating program..")
-    # Close the plot when Ctrl-C is pressed
-    # plt.close()
-    # Turn off the LED
-    print("Turning off LED..")
+    # Cleanup
     board.digital[led_blue_pin].write(0)
     board.digital[led_red_pin].write(0)
-    # Close the serial connection to the Arduino board
-    print("Closing serial connection to Arduino board..")
     board.exit()
     print("Program terminated")
-    
-    
